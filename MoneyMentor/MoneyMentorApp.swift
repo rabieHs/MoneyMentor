58a9909d6951e7ec843840e3fc1be88fcdcc6ba7
rabieHs <rabiehoussaini@gmail.com>
2024-07-16 12:58:06 +0100

    Initial Commit

---
+++ MoneyMentor/MoneyMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMentorApp.swift
+//  MoneyMentor
+//
+//  Created by rabie houssaini on 16/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
